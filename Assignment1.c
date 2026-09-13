fadedf1a4d8a2450f4839e26e43dc6a14c3fbe56
jeremykovulo-lgtm <jeremykovulo@gmail.com>
2026-09-13 21:13:13 +0300

    Add files via upload

---
+++ Assignment1.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(){
+    printf("1.Introduction to programming \n");
+    printf("2.Data Structures \n");
+    printf("3.Computer Networks \n");
+    printf("4.Database Systems \n");
+    printf("5.Software Engineering");
+ 
+}
