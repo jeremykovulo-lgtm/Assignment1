0d5c62f5535eccc16161b7d74de8bb028144ccb7
jeremykovulo-lgtm <jeremykovulo@gmail.com>
2026-09-19 23:48:04 +0300

    Revise program to input and display user data
    Updated the program to collect user input for height, bank balance, and phone number, replacing the previous static output.

--- week1_Assignmet2.c
+++ week1_Assignmet2.c
@@ -2,11 +2,20 @@
   Admission number:BCS-05-0070/2026
     */
 #include <stdio.h>
+float height;
+double bank_balance;
+char phone_number[15];
+
 int main(){
-    printf("1.Digital literacy");
-    printf("2.Structured Programming");
-    printf("3.Basic mathematics");
-    printf("Computaion Logic");
-    printf("Introduction to Physics");
-  return 0;
+   printf("Enter your height in metres:\t");
+   scanf("%f",&height);
+   printf("Enter your bank balance in Kenyan shillings:\t");
+   scanf("%lf",&bank_balance);
+   printf("Enter your phone number:\t");
+   scanf("%s",phone_number);
+
+   printf("Your height is %f",height);
+   printf("Your bank balance is %lf",bank_balance);
+   printf("Your phone number is %s",phone_number);
+   return 0;
 }
