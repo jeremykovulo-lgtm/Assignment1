51e8e75761bde2bc559fdea87bd682efd72003a2
jeremykovulo-lgtm <jeremykovulo@gmail.com>
2026-09-19 20:37:31 +0300

    Update Assignment1.c

--- Assignment1.c
+++ Assignment1.c
@@ -1,12 +1,12 @@
-#include <stdio.h>
 /*Name: Jeremy Kovulo
   Admission number:BCS-05-0070/2026
     */
+#include <stdio.h>
 int main(){
-    printf("1.Introduction to programming \n");
-    printf("2.Data Structures \n");
-    printf("3.Computer Networks \n");
-    printf("4.Database Systems \n");
-    printf("5.Software Engineering");
- return 0;
+    printf("1.Digital literacy");
+    printf("2.Structured Programming");
+    printf("3.Basic mathematics");
+    printf("Computaion Logic");
+    printf("Introduction to Physics");
+  return 0;
 }
