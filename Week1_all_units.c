a0fd78a8dfa523548dc0442a9080fabd57a85a1c
jeremykovulo-lgtm <jeremykovulo@gmail.com>
2026-09-19 20:30:51 +0300

    Add files via upload
    Week1 Assignment

---
+++ Week1_all_units.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main(){
+    printf("1.Digital literacy");
+    printf("2.Structured Programming");
+    printf("3.Basic mathematics");
+    printf("Computaion Logic");
+    printf("Introduction to Physics");
+}
