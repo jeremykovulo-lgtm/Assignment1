0b5d4fc6466caa19d9cfa1a517a919930357e9fc
jeremykovulo-lgtm <jeremykovulo@gmail.com>
2026-09-20 00:17:45 +0300

    Add files via upload

---
+++ Week2_Assignment1.c
@@ -0,0 +1,18 @@
+/*Name: Jeremy Kovulo
+  Admission number:BCS-05-0070/2026
+    */
+#include <stdio.h>
+#include <math.h>
+float pie=3.142;
+float radius;
+float height;
+int main(){
+ printf("Enter radius of the cylinder:\t");
+ scanf("%f",&radius);
+ printf("Enter height of the cylinder:\t");
+ scanf("%f",&height);
+ float Volume=pie*pow(radius,2)*height;
+ float Surface_area=(pie*pow(radius,2))+(2*pie*radius*height);
+ printf("Volume =\t%f",Volume);
+ printf("Surface area =\t%f",Surface_area);
+}
